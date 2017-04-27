075719262cc04be3b62750e31ad856c83399920d
administrator <liquidnuker@users.noreply.github.com>
2017-04-27 11:18:14 -0700

    trying different setup of updating state...

--- src/vue-components/bonsai.vue
+++ src/vue-components/bonsai.vue
@@ -18,61 +18,38 @@ import {mediator} from "../js/mediator.js";
 
 export default {
   data() {
-      return {
-        items: store.state
-      }
-    },
-    computed: {
-      gr: function () {
-        this.items = store.state;
-      }
-    },
-    beforeCreate: function () {
-      // todo: insert loading v-if
-    },
-    created: function () {
-      console.log("created");
-      // this.items = store.state;
-      this.loadCheck();
-    },
-    updated: function () {
-      console.log("updated");
-      this.loadCheck();
-    },
-    mounted: function () {
-      console.log("mounted");
-    },
-    methods: {
-      loadCheck: function () {
-        if (!this.$route.params.species) {
-          // console.log("no species: " + !this.$route.params.species + " load all");
-
-          // load all
-          return new Promise(function (resolve, reject) {
-
-            const itemstore = mediator.publish("loadAll");
-            resolve(itemstore);
-            // reject(Error("error"));
-
-          }).then(function (resolved) {
-            // success
-            console.log("resolved");
-
-
-          }, function (err) {
-            console.log(err); // error
-          });
-
-
-        } else if (!this.$route.params.id) {
-          // console.log("no id. just load species");
-        } else {
-          // console.log("load species then id");
-        }
-      }, // end loadcheck 
-      customUrl: function () {
-        router.push('/bonsai/:87956876/:ofgfjgfgh')
-      }
+    return {
+      items: store.state
     }
+  },
+  beforeCreate: function () {
+    console.log("beforeCreate");
+  },
+  created: function () {
+    console.log("created");
+    // this.items = store.state;
+    this.loadCheck();
+  },
+  updated: function () {
+    console.log("updated");
+  },
+  mounted: function () {
+    console.log("mounted");
+  },
+  methods: {
+    loadCheck: function () {
+      if (!this.$route.params.species) {
+        console.log("no species: " + !this.$route.params.species + " load all");
+        // load all
+      } else if (!this.$route.params.id) {
+        console.log("no id. just load species");
+      } else {
+        console.log("load species then id");
+      }
+    }, // end loadcheck 
+    customUrl: function () {
+      router.push('/bonsai/:87956876/:ofgfjgfgh')
+    }
+  }
 }
 </script>
