8a2ac332cfc978919dd0bdf890899e853ac16013
administrator <liquidnuker@users.noreply.github.com>
2017-04-27 20:20:14 -0700

    v-for added

--- src/vue-components/bonsai.vue
+++ src/vue-components/bonsai.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="vc-page-bonsai" class="vc-page-bonsai">
     <p>vc-page-bonsai</p>
-    <br>
-    <p>
-    {{ items }}
-    </p>
+    <div id="galleryContainer">
+      <div v-for="i in items">
+        <div class="col-sm-12 col-sm-4">
+          {{ i.id }} <br>
+          {{ i.thumb }} <br>
+          <img v-bind:src="'img/' + i.id +'.jpg'" v-bind:id="i.id">
+        </div>
+      </div>
+    </div>
     <p>params: {{$route.params}}</p>
-  <button v-on:click="customUrl">custom url</button>
+    <button v-on:click="customUrl">custom url</button>
   </div>
-
 </template>
 
 <script>
